4efdffc7478f1cd5514649468896f6aa67a3a8bf
CuriousOne23 <jeffganger@gmail.com>
2026-07-22 07:05:00 -0700

    Create init.py

---
+++ thought_simulator/20_requirements/system_playground/context/init.py
@@ -0,0 +1,14 @@
+"""
+Context subsystem package for system_playground.
+
+Provides:
+- CST (core, ms, mux)
+- COB (identity evolution)
+- CIL (intake packet construction)
+"""
+
+from . import cst_core
+from . import cst_ms
+from . import cst_mux
+from . import cob
+from . import cil
