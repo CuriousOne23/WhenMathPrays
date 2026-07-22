827c9db0e3191d5867555aa5af1edff67ff75241
CuriousOne23 <jeffganger@gmail.com>
2026-07-22 07:07:24 -0700

    Add docstring to context package for clarity
    Added docstring to provide context about the package and its components.

--- thought_simulator/20_requirements/system_playground/context/__init__.py
+++ thought_simulator/20_requirements/system_playground/context/__init__.py
@@ -1,4 +1,15 @@
-context/__init__.py
-context/cst/__init__.py
-context/cil/__init__.py
-context/cob/__init__.py
+"""
+Context subsystem package for system_playground.
+
+Provides:
+- CST (core, ms, mux)
+- COB (identity evolution)
+- CIL (intake packet construction)
+"""
+
+from . import cst_core
+from . import cst_ms
+from . import cst_mux
+from . import cob
+from . import cil
+
