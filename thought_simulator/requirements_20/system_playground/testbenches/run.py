c4ff5d5b45ce015395497cbeda80e7a704b85019
CuriousOne23 <jeffganger@gmail.com>
2026-07-27 11:29:15 -0700

    Update run.py

--- thought_simulator/requirements_20/system_playground/testbenches/run.py
+++ thought_simulator/requirements_20/system_playground/testbenches/run.py
@@ -8,6 +8,8 @@ This runner:
     • Calls each testbench's run_testbench() function directly
     • Does NOT use unittest (development mode)
 """
+import sys
+sys.stdout.reconfigure(encoding='utf-8')
 
 import sys
 import os
