1aa0e3d0a6cc0c8021cef04195219f5d77dddfdc
CuriousOne23 <jeffganger@gmail.com>
2026-07-27 12:29:32 -0700

    Modify testbench configurations for InB and IIInB
    Updated testbench configuration to include IIInB test bench and adjust InB test bench settings.

--- thought_simulator/requirements_20/system_playground/testbenches/run.py
+++ thought_simulator/requirements_20/system_playground/testbenches/run.py
@@ -24,21 +24,65 @@ sys.path.insert(0, repo_root)
 # ============================================================
 # SELECT TESTBENCHES TO RUN (USER EDITS THIS SECTION)
 # ============================================================
-
 ACTIVE_TEST_MODULES = [
+    # **************************** InB Test bench ****************************************************
+    # (
+    #     "thought_simulator.requirements_20.system_playground.testbenches.path_a.intake.inb_testbench",
+    #     {
+    #         "mode": "standalone",
+    #         "use_inb": True,
+    #         "use_iiinb": False,
+    #         "use_ie": False,
+    #         "tests_to_run": {
+    #             "inb_baseline_no_error": "Yes",
+    #             "inb_whitespace_anomaly": "Yes",
+    #             "inb_multiple_surface_anomalies": "Yes",
+    #             "inb_illegal_character_only": "Yes",
+    #             "inb_mixed_whitespace_illegal": "Yes",
+    #             "inb_structural_tags_present": "Yes",
+    #             "inb_tokenizable_input": "Yes",
+    #             "inb_unicode_normalization_opportunity": "Yes",
+    #             "inb_complex_mixed_case": "Yes"
+    #         }
+    #     }
+    # ),
+    
+    # **************************** IIInB Test bench ****************************************************
+    (
+        "thought_simulator.requirements_20.system_playground.testbenches.path_a.intake.iiinb_testbench",
+        {
+            # Pipeline mode: "standalone" or "progressive"
+            "mode": "standalone",
+
+            # Upstream toggles: User inputs True or False
+            "use_inb": True,
+            "use_iiinb": True,
+            "use_ie": False,
+
+            # tests_to_run: keys MUST match iiinb_testbench.yaml `id` fields
+            "tests_to_run": {
+                "iiinb_unicode_normalization": "Yes",
+                "iiinb_structural_cleanup": "Yes",
+                "iiinb_shorthand_expansion": "Yes",
+                "iiinb_spelling_transposed": "Yes",
+                "iiinb_spelling_missing_letter": "Yes",
+                "iiinb_repetition_collapse": "Yes",
+                "iiinb_illegal_character_anomaly": "Yes",
+                "iiinb_mixed_repairs_anomalies": "Yes",
+                "iiinb_tokens_emitted": "Yes",
+                "iiinb_structure_tags_empty": "Yes"
+            }
+        }
+    ),
+
     # **************************** IE Test bench ******************************************************
     # (
     #     "thought_simulator.requirements_20.system_playground.testbenches.path_a.intake.ie_testbench",
     #     {
-    #         # Pipeline mode: "standalone" or "progressive"
     #         "mode": "standalone",
-    #
-    #         # Upstream toggles: User inputs True or False
     #         "use_inb": False,
     #         "use_iiinb": False,
     #         "use_ie": True,
-    #
-    #         # tests_to_run: keys MUST match ie_testbench.yaml `id` fields
     #         "tests_to_run": {
     #             "ie_repair_only_whitespace": "Yes",
     #             "ie_multiple_repairs": "Yes",
@@ -50,35 +94,7 @@ ACTIVE_TEST_MODULES = [
     #             "ie_complex_mixed": "Yes"
     #         }
     #     }
-    # ),
-    # **************************** InB Test bench ****************************************************
-    (
-        "thought_simulator.requirements_20.system_playground.testbenches.path_a.intake.inb_testbench",
-        {
-            # Pipeline mode: "standalone" or "progressive"
-            "mode": "standalone",
-
-            # Upstream toggles: User inputs True or False
-            "use_inb": True,
-            "use_iiinb": False,
-            "use_ie": False,
-
-            # tests_to_run: keys MUST match inb_testbench.yaml `id` fields
-            "tests_to_run": {
-                "inb_baseline_no_error": "Yes",
-                "inb_whitespace_anomaly": "Yes",
-                "inb_multiple_surface_anomalies": "Yes",
-                "inb_illegal_character_only": "Yes",
-                "inb_mixed_whitespace_illegal": "Yes",
-                "inb_structural_tags_present": "Yes",
-                "inb_tokenizable_input": "Yes",
-                "inb_unicode_normalization_opportunity": "Yes",
-                "inb_complex_mixed_case": "Yes"
-            }
-        }
-    ),
-
-    # Add more testbenches here later
+    # )
 ]
 
 # ============================================================
