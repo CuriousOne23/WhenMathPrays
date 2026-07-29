6399b72ee3cb347ab9c006e7cf49f325859fb338
CuriousOne23 <jeffganger@gmail.com>
2026-07-28 18:21:25 -0700

    Update tests_to_run to reference YAML file

--- thought_simulator/requirements_20/system_playground/testbenches/run.py
+++ thought_simulator/requirements_20/system_playground/testbenches/run.py
@@ -35,17 +35,7 @@ ACTIVE_TEST_MODULES = [
             "use_inb": True,
             "use_iiinb": False,
             "use_ie": False,
-            "tests_to_run": {
-                "inb_baseline_no_error": "Yes",
-                "inb_whitespace_anomaly": "Yes",
-                "inb_multiple_surface_anomalies": "Yes",
-                "inb_illegal_character_only": "Yes",
-                "inb_mixed_whitespace_illegal": "Yes",
-                "inb_structural_tags_present": "Yes",
-                "inb_tokenizable_input": "Yes",
-                "inb_unicode_normalization_opportunity": "Yes",
-                "inb_complex_mixed_case": "Yes"
-            }
+            "tests_to_run": "see inb_tests_to_run.yaml"
         }
     ),
 
