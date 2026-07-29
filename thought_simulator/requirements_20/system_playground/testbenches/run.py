90483d9091e673fd7c180099506e1693d1eebaad
CuriousOne23 <jeffganger@gmail.com>
2026-07-29 13:31:00 -0700

    Update run.py

--- thought_simulator/requirements_20/system_playground/testbenches/run.py
+++ thought_simulator/requirements_20/system_playground/testbenches/run.py
@@ -45,7 +45,7 @@ ACTIVE_TEST_MODULES = [
     (
         "thought_simulator.requirements_20.system_playground.testbenches.path_a.intake.iiinb_testbench",
         {
-            "mode": "testbench",   # or "general"
+            "mode": "general",   # or "general"
             "use_inb": False,      # Upstream InB ignored
             "use_iiinb": True,     # Primitive under test
             "use_ie": False,       # Downstream IE ignored
