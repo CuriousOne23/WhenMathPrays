b5be5f47631db03b2be5607c0858ed9bbb478223
CuriousOne23 <jeffganger@gmail.com>
2026-08-07 16:49:39 -0700

    Add projection provenance to cil_meta

--- thought_simulator/requirements_20/system_playground/primitives/cex_pck/cex_pck.py
+++ thought_simulator/requirements_20/system_playground/primitives/cex_pck/cex_pck.py
@@ -129,7 +129,7 @@ class CExPck:
         cil_meta = {
             "selected_conversation": ccr.get("selected_conversation"),
             "cil_reference": "cil_input.yaml",
-            "provenance": {                     # <-- MUST be "provenance"
+            "projection_provenance": {
                 "origin": "CEx-CCR",
                 "packaged_by": "CEx-Pck"
             }
