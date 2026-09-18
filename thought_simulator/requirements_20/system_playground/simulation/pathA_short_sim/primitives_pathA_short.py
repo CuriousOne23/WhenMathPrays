1c1bfc5638cceeaa9679bc68d124f078b8478b86
CuriousOne23 <jeffganger@gmail.com>
2026-09-18 14:56:11 -0700

    Create primitives_pathA_short.py

---
+++ thought_simulator/requirements_20/system_playground/simulation/pathA_short_sim/primitives_pathA_short.py
@@ -0,0 +1,25 @@
+type PrimitiveFn = (tp: TP) => TP
+
+function InB(tp: TP): TP {}
+function IIInB(tp: TP): TP {}
+function IE(tp: TP): TP {}
+
+function CEx(tp: TP): TP {}
+function CE(tp: TP): TP {}
+function ISc(tp: TP): TP {}
+function TPU(tp: TP): TP {}
+
+function SOB(tp: TP): TP {}
+function SROB(tp: TP): TP {}
+function CnOB(tp: TP): TP {}
+function SmOB(tp: TP): TP {}
+function SSG(tp: TP): TP {}
+
+function RBU(tp: TP): TP {}
+function RB(tp: TP): TP {}
+function RTU(tp: TP): TP {}
+function CTP(tp: TP): TP {}
+
+function IdOB(tp: TP): TP {}
+function TR(tp: TP): TP {}
+function OuBA(tp: TP): TP {}
